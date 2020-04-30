63fcf071ca1859d98d1909ce104e88e9919e0efb
clintonoliera <clintonoliera391@gmail.com>
2020-04-30 15:08:47 +0300

    added a division function to script.js file

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,11 @@ var add = function(number1,number2){
     var number2 = parseInt(prompt("Enter another number"));
     var result = add(number1,number2);
     alert(result);
+
+    var divide = function(number1,number2) {
+        return(number1 / number2);
+    };
+    var number1 = parseInt(prompt("Enter your number here"));
+    var number2 = parseInt(prompt("Enter another number"));
+    var result = divide(number1,number2);
+    alert(result);
